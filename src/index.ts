40336a9c328072ada2b7b6f806662651c22bf8bd
jphneves <jphneves@yahoo.com>
2025-06-23 20:33:44 -0300

    versao 3.3

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ declare global {
 const prisma = global.prisma || new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL + "?pgbouncer=true&connection_limit=1",
     },
   },
 });
